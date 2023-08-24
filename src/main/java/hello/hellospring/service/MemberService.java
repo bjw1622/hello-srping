e009d46c52a0a6df69c100f7569845c2f9cefd9f
백재원 <bjw1622@gmail.com>
2023-08-24 09:49:27 +0900

    :art: feat : MemberService DI

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -2,13 +2,20 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    // MemberRepository 직접 만드는게 아닌 외부에서 넣어주는 것
+    // 내가 직접 new 하지 않음
+    // 이게 바로 di (외부에서 넣어줌)
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
 
     /*
      *  회원 가입
